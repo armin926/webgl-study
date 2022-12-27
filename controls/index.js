49b8dc101bb553c7a0880f6f90dcaaf42f9bbfd0
wangchunmin <122699029@qq.com>
2022-12-27 14:00:17 +0800

    feat: three光源

---
+++ controls/index.js
@@ -0,0 +1,65 @@
+const basicType = {
+  color: {
+    method: "addColor",
+    getValue: (item) => item.color.getStyle(),
+    setValue: (item, value) => item.color.setStyle(value),
+  },
+  // skyColor: {
+  //   method: "addColor",
+  //   getValue: (item) => item.skyColor.getStyle(),
+  //   setValue: (item, value) => item.skyColor.setStyle(value),
+  // },
+  groundColor: {
+    method: "addColor",
+    getValue: (item) => item.groundColor.getStyle(),
+    setValue: (item, value) => item.groundColor.setStyle(value),
+  },
+  intensity: {
+    extends: [0, 10],
+    getValue: (item) => item.intensity,
+    setValue: (item, value) => item.intensity = +value,
+  },
+  distance: {
+    extends: [0, 2],
+    getValue: (item) => item.distance,
+    setValue: (item, value) => item.distance = +value,
+  },
+  angle: {
+    extends: [0, Math.PI / 2],
+    getValue: (item) => item.angle,
+    setValue: (item, value) => item.angle = +value,
+  },
+  exponent: {
+    extends: [0, 20],
+    getValue: (item) => item.exponent,
+    setValue: (item, value) => item.exponent = +value,
+  },
+}
+
+const itemType = {
+  SpotLight: ['color', 'intensity', 'distance', 'angle', 'exponent'], // 聚光灯
+  AmbientLight: ['color'], // 环境光
+  PointLight: ['color', 'intensity', 'distance'], // 点光源
+  DirectionalLight: ['color', 'intensity'], // 平行光
+  HemisphereLight: ['skycolor', 'groundColor', 'intensity'], // 半球光
+}
+
+function initControls(item) {
+  const typeList = itemType[item.type]
+  const controls = {}
+  if (!typeList || !typeList.length) {
+    return
+  }
+  const gui = new dat.GUI()
+  for(let i = 0; i < typeList.length; i++) {
+    const child = basicType[typeList[i]]
+    if (child) {
+      controls[typeList[i]] = child.getValue(item)
+
+      const childExtends = child.extends || []
+      gui[child.method || 'add'](controls, typeList[i], ...childExtends).onChange((value) => {
+        child.setValue(item, value)
+      })
+    }
+  }
+}
